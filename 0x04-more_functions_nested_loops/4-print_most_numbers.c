907cae9c69a2df743e2344bc99787daa24dcc7c2
iamnotgreyghost <raphaelogbuabor@gmail.com>
2022-06-14 14:44:24 -0700

    fofo

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints number
+ */
+
+void print_most_numbers(void)
+
+{
+
+	int a;
+
+	for (a = 48; a < 58; a++)
+
+	{
+
+		if (a != 50 && a != 52)
+
+		{
+
+			_putchar(a);
+
+		}
+
+	}
+
+	_putchar('\n');
+
+}
