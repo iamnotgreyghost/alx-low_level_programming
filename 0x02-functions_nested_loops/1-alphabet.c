019fcaeb2539d936e99e9436b0aeff4a8210e8cf
iamnotgreyghost <raphaelogbuabor@gmail.com>
2022-06-14 10:43:11 -0700

    good

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabet in small letters
+ *
+ * Return: 0
+ */
+
+void print_alphabet(void)
+
+{
+
+	char k = 'a';
+
+	while (k <= 'z')
+
+	{
+
+		_putchar(k);
+
+		k++;
+
+	}
+
+	_putchar('\n');
+
+}
