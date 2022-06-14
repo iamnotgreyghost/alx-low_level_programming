7c3a97c77717d446435cd1d25efbade499962018
iamnotgreyghost <raphaelogbuabor@gmail.com>
2022-06-14 12:27:18 -0700

    betty

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: always 0
+ */
+int main(void)
+
+{
+	long int a, b, c, d;
+
+	b = 1;
+	c = 2;
+	for (a = 1; a <= 50; ++a)
+	{
+		if (b != 20365011074)
+		{
+			printf("%ld, ", b);
+		} else
+		{
+			printf("%ld\n", b);
+		}
+		d = b + c;
+		b = c;
+		c = d;
+	}
+	return (0);
+}
