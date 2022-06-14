8aa839858c371e575db62984c4a392e342678b58
iamnotgreyghost <raphaelogbuabor@gmail.com>
2022-06-14 14:41:48 -0700

    jiji

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints number
+ */
+
+void print_numbers(void)
+
+{
+
+	int a;
+
+	for (a = 48; a < 58; a++)
+
+	{
+
+		_putchar(a);
+
+	}
+
+	_putchar('\n');
+
+}
