6e403f1df211585cc62851075ce8f2ee77a9faa3
iamnotgreyghost <raphaelogbuabor@gmail.com>
2022-06-16 14:24:33 -0700

    juj

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * string_toupper - coverts lowercase letters of a string to lowercase
+ * @s: string to be modified
+ *
+ * Return: the resulting string
+ */
+
+char *string_toupper(char *s)
+
+{
+
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+
+	{
+
+		if (s[i] >= 'a' && s[i] <= 'z')
+
+			s[i] = s[i] - 32;
+
+	}
+
+	return (s);
+
+}
